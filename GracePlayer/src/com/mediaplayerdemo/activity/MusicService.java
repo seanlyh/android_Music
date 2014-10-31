055a138d3a82efaef99fe588010b196f171d9a08
seanlyh <453633317@qq.com>
2014-11-01 00:04:01 +0800

    2014.11

--- GracePlayer/src/com/mediaplayerdemo/activity/MusicService.java
+++ GracePlayer/src/com/mediaplayerdemo/activity/MusicService.java
@@ -16,180 +16,180 @@ import android.widget.Toast;
 
 
 public class MusicService extends Service {
-	@Override
-	public IBinder onBind(Intent intent) {
-		
-		return null;
-	}
-	public static final int COMMAND_UNKNOWN = -1;
-	public static final int COMMAND_PLAY = 0;
-	public static final int COMMAND_PAUSE = 1;
-	public static final int COMMAND_STOP = 2;
-	public static final int COMMAND_RESUME = 3;
-	public static final int COMMAND_PREVIOUS = 4;
-	public static final int COMMAND_NEXT = 5 ;
-	public static final int COMMAND_CHECK_IS_PLAYING = 6;
-	public static final int COMMAND_SEEK_TO = 7;
-	
-	public static final int STATUS_PLAYING = 0;
-	public static final int STATUS_PAUSED = 1;
-	public static final int STATUS_STOPPED = 2;
-	public static final int STATUS_COMPLETED = 3;
-	
-	public static final String BROADCAST_MUSICSERVICE_CONTROL = "MusicService.ACTION_CONTROL" ;
-	public static final String BROADCAST_MUSICSERVICE_UPDATE_STATUS = "MusicService.ACTION_UPDATE";
-	
-	private MediaPlayer player2;
-	
-	private CommandReceiver receiver;
-	
-	@Override
-	public void onCreate(){
-		super.onCreate();
-		//°ó¶¨¹ã²¥½ÓÊÕÆ÷£¬¿ÉÒÔ½ÓÊÕ¹ã²¥
-		bindCommandReceiver();
-		Toast.makeText(this, "MusicService.onCreate()", Toast.LENGTH_SHORT)
-		.show();
-	}
-	@Override
-	public void onStart(Intent intent,int startId){
-		super.onStart(intent, startId);
-	}
-	@Override
-	public void onDestroy(){
-		//ÊÍ·Å²¥·ÅÆ÷×ÊÔ´
-		if(player2 !=null){
-			player2.release();
-		}
-		super.onDestroy();
-	}
-	/**°ó¶¨¹ã²¥½ÓÊÕÆ÷*/
-	private void bindCommandReceiver(){
-		receiver = new CommandReceiver();
-		IntentFilter filter = new IntentFilter(BROADCAST_MUSICSERVICE_CONTROL);
-		registerReceiver(receiver,filter);
-	}
-	
-	/**½ÓÊÕ¹ã²¥ÃüÁî£¬²¢Ö´ĞĞ*/
-	class CommandReceiver extends BroadcastReceiver{
-		public void onReceive(Context context, Intent intent) {
-			// »ñÈ¡ÃüÁîĞĞ
-			int command = intent.getIntExtra("command", COMMAND_UNKNOWN);
-			//Ö´ĞĞÃüÁî
-			switch (command){
-			case COMMAND_SEEK_TO:
-				seekTo(intent.getIntExtra("time", 0));
-				break;
-			case COMMAND_PLAY:
-			case COMMAND_PREVIOUS:				
-			case COMMAND_NEXT:
-				int number = intent.getIntExtra("number", 1);
-				Toast.makeText(MusicService.this,"ÕıÔÚ²¥·ÅµÚ" + number + "Ê×" , Toast.LENGTH_SHORT).show();
-				play(number);
-				break;
-			case COMMAND_PAUSE:
-				pause();
-				break;
-			case COMMAND_RESUME:
-				resume();
-				break;
-			case COMMAND_STOP:
-				stop();
-				break;
-			case COMMAND_CHECK_IS_PLAYING:
-				if(player2.isPlaying())
-				{
-					sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
-				}
-				break;
-			case COMMAND_UNKNOWN:
-				default:
-					break;
-			}
-		}
-		
-	}
-	/**·¢ËÍ¹ã²¥£¬ÌáĞÑ×´Ì¬¸Ä±äÁË*/
-	private void sendBroadcastOnStatusChanged(int status){
-		Intent intent = new Intent(BROADCAST_MUSICSERVICE_UPDATE_STATUS);
-		intent.putExtra("status", status);
-		
-		if (status == STATUS_PLAYING) {
-			intent.putExtra("time", player2.getCurrentPosition());
-			intent.putExtra("duration", player2.getDuration());
-		}
-		
-		sendBroadcast(intent);
-	}
-	
-	
-	/**¶ÁÈ¡ÒôÀÖÎÄ¼ş*/
-	private void load(int number){
-		if (player2 != null){
-			player2.release();
-		}
-		Uri musicUri = Uri.withAppendedPath(
-				MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, "" + number);
-		//¶ÁÈ¡ÒôÀÖÎÄ¼ş£¬´´½¨MediaPlayer¶ÔÏó
-		player2 = MediaPlayer.create(this, musicUri);
-		//×¢²á¼àÌıÆ÷
-		player2.setOnCompletionListener(completionListener);
-	}
-	
-	//²¥·Å½áÊø¼àÌıÆ÷
-	OnCompletionListener completionListener = new OnCompletionListener() {
-		@Override
-		public void onCompletion(MediaPlayer player){
-			if(player.isLooping()){
-				replay();
-			} else {
-				sendBroadcastOnStatusChanged(MusicService.STATUS_COMPLETED);
-			}
-		}
-	};
-	
-	/**²¥·ÅÒôÀÖ*/
-	private void play(int number){
-		//Í£Ö¹µ±Ç°²¥·Å
-		if(player2 != null && player2.isPlaying()){
-			player2.stop();
-		}
-		load(number);
-		player2.start();
-		sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
-	}
-	
-	/**ÔİÍ£ÒôÀÖ*/
-	private void pause(){
-		if(player2.isPlaying()){
-			player2.pause();
-			sendBroadcastOnStatusChanged(MusicService.STATUS_PAUSED);
-		}	
-	}
-	
-	private void stop(){
-		if(player2 != null){
-			player2.stop();
-			sendBroadcastOnStatusChanged(MusicService.STATUS_STOPPED);
-		}
-	}
-	
-	private void resume(){
-		player2.start();
-		sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
-	}
-	
-	/**Íê³ÉÖ®ºóÖØĞÂ²¥·Å*/
-	private void replay(){
-		player2.start();
-		sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
-	}
-	
-	//Ìø×ªÖÁ²¥·ÅÎ»ÖÃ
-	private void seekTo(int time) {
-		if(player2 != null) {
-			player2.seekTo(time);
-		}
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+    public static final int COMMAND_UNKNOWN = -1;
+    public static final int COMMAND_PLAY = 0;
+    public static final int COMMAND_PAUSE = 1;
+    public static final int COMMAND_STOP = 2;
+    public static final int COMMAND_RESUME = 3;
+    public static final int COMMAND_PREVIOUS = 4;
+    public static final int COMMAND_NEXT = 5 ;
+    public static final int COMMAND_CHECK_IS_PLAYING = 6;
+    public static final int COMMAND_SEEK_TO = 7;
+
+    public static final int STATUS_PLAYING = 0;
+    public static final int STATUS_PAUSED = 1;
+    public static final int STATUS_STOPPED = 2;
+    public static final int STATUS_COMPLETED = 3;
+
+    public static final String BROADCAST_MUSICSERVICE_CONTROL = "MusicService.ACTION_CONTROL" ;
+    public static final String BROADCAST_MUSICSERVICE_UPDATE_STATUS = "MusicService.ACTION_UPDATE";
+
+    private MediaPlayer player2;
+
+    private CommandReceiver receiver;
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        //ç»‘å®šå¹¿æ’­æ¥æ”¶å™¨ï¼Œå¯ä»¥æ¥æ”¶å¹¿æ’­
+        bindCommandReceiver();
+        Toast.makeText(this, "MusicService.onCreate()", Toast.LENGTH_SHORT)
+                .show();
+    }
+    @Override
+    public void onStart(Intent intent,int startId){
+        super.onStart(intent, startId);
+    }
+    @Override
+    public void onDestroy(){
+        //é‡Šæ”¾æ’­æ”¾å™¨èµ„æº
+        if(player2 !=null){
+            player2.release();
+        }
+        super.onDestroy();
+    }
+    /**ç»‘å®šå¹¿æ’­æ¥æ”¶å™¨*/
+    private void bindCommandReceiver(){
+        receiver = new CommandReceiver();
+        IntentFilter filter = new IntentFilter(BROADCAST_MUSICSERVICE_CONTROL);
+        registerReceiver(receiver,filter);
+    }
+
+    /**æ¥æ”¶å¹¿æ’­å‘½ä»¤ï¼Œå¹¶æ‰§è¡Œ*/
+    class CommandReceiver extends BroadcastReceiver{
+        public void onReceive(Context context, Intent intent) {
+            // è·å–å‘½ä»¤è¡Œ
+            int command = intent.getIntExtra("command", COMMAND_UNKNOWN);
+            //æ‰§è¡Œå‘½ä»¤
+            switch (command){
+                case COMMAND_SEEK_TO:
+                    seekTo(intent.getIntExtra("time", 0));
+                    break;
+                case COMMAND_PLAY:
+                case COMMAND_PREVIOUS:
+                case COMMAND_NEXT:
+                    int number = intent.getIntExtra("number", 1);
+                    Toast.makeText(MusicService.this,"æ­£åœ¨æ’­æ”¾ç¬¬" + number + "é¦–" , Toast.LENGTH_SHORT).show();
+                    play(number);
+                    break;
+                case COMMAND_PAUSE:
+                    pause();
+                    break;
+                case COMMAND_RESUME:
+                    resume();
+                    break;
+                case COMMAND_STOP:
+                    stop();
+                    break;
+                case COMMAND_CHECK_IS_PLAYING:
+                    if(player2.isPlaying())
+                    {
+                        sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
+                    }
+                    break;
+                case COMMAND_UNKNOWN:
+                default:
+                    break;
+            }
+        }
+
+    }
+    /**å‘é€å¹¿æ’­ï¼Œæé†’çŠ¶æ€æ”¹å˜äº†*/
+    private void sendBroadcastOnStatusChanged(int status){
+        Intent intent = new Intent(BROADCAST_MUSICSERVICE_UPDATE_STATUS);
+        intent.putExtra("status", status);
+
+        if (status == STATUS_PLAYING) {
+            intent.putExtra("time", player2.getCurrentPosition());
+            intent.putExtra("duration", player2.getDuration());
+        }
+
+        sendBroadcast(intent);
+    }
+
+
+    /**è¯»å–éŸ³ä¹æ–‡ä»¶*/
+    private void load(int number){
+        if (player2 != null){
+            player2.release();
+        }
+        Uri musicUri = Uri.withAppendedPath(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, "" + number);
+        //è¯»å–éŸ³ä¹æ–‡ä»¶ï¼Œåˆ›å»ºMediaPlayerå¯¹è±¡
+        player2 = MediaPlayer.create(this, musicUri);
+        //æ³¨å†Œç›‘å¬å™¨
+        player2.setOnCompletionListener(completionListener);
+    }
+
+    //æ’­æ”¾ç»“æŸç›‘å¬å™¨
+    OnCompletionListener completionListener = new OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer player){
+            if(player.isLooping()){
+                replay();
+            } else {
+                sendBroadcastOnStatusChanged(MusicService.STATUS_COMPLETED);
+            }
+        }
+    };
+
+    /**æ’­æ”¾éŸ³ä¹*/
+    private void play(int number){
+        //åœæ­¢å½“å‰æ’­æ”¾
+        if(player2 != null && player2.isPlaying()){
+            player2.stop();
+        }
+        load(number);
+        player2.start();
+        sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
+    }
+
+    /**æš‚åœéŸ³ä¹*/
+    private void pause(){
+        if(player2.isPlaying()){
+            player2.pause();
+            sendBroadcastOnStatusChanged(MusicService.STATUS_PAUSED);
+        }
+    }
+
+    private void stop(){
+        if(player2 != null){
+            player2.stop();
+            sendBroadcastOnStatusChanged(MusicService.STATUS_STOPPED);
+        }
+    }
+
+    private void resume(){
+        player2.start();
+        sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
+    }
+
+    /**å®Œæˆä¹‹åé‡æ–°æ’­æ”¾*/
+    private void replay(){
+        player2.start();
+        sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
+    }
+
+    //è·³è½¬è‡³æ’­æ”¾ä½ç½®
+    private void seekTo(int time) {
+        if(player2 != null) {
+            player2.seekTo(time);
+        }
+    }
 
 }
--- GracePlayer/src/com/mediaplayerdemo/activity/PropertyBean.java
+++ GracePlayer/src/com/mediaplayerdemo/activity/PropertyBean.java
@@ -14,54 +14,54 @@ import android.os.IBinder;
 
 
 public class PropertyBean  {
-  public static String[] THEMES;
-  private static String DEFAULT_THEME;
-  private Context context;
-  private String theme; 
-  
-  public PropertyBean(Context context) {
-	  this.context = context;
-	  THEMES = context.getResources().getStringArray(R.array.theme);
-	  DEFAULT_THEME = THEMES[0];
-	  loadTheme();
-  }
-  //∂¡»°÷˜Ã‚°£±£¥Ê‘⁄Œƒº˛configuration.cfg÷–
-  private void loadTheme() {
-	  Properties properties = new Properties(); 
-	  try {
-		  FileInputStream stream = context.openFileInput("configuration.cfg");
-		  properties.load(stream);
-		  theme = properties.getProperty("theme").toString();
-	  } catch (Exception e) {
-		  saveTheme(DEFAULT_THEME);
-	  }
-  }	
-  
-  //±£¥Ê÷˜Ã‚°£±£¥Ê‘⁄Œƒº˛configuration.cfg÷–
-	private boolean saveTheme(String theme) {
-		Properties properties = new Properties();
-		properties.put("theme", theme);
-		try {
-			FileOutputStream stream = context.openFileOutput(
-					"configuration.cfg", Context.MODE_WORLD_WRITEABLE);
-			properties.store(stream,"");
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}  
-	
+    public static String[] THEMES;
+    private static String DEFAULT_THEME;
+    private Context context;
+    private String theme;
 
-		  public String getTheme() { 
-			  return theme;
-		  }
-		  
-		  public void setAndSaveTheme(String theme) {
-			  this.theme = theme;
-			  saveTheme(theme);
-		  }
-	 
-  
+    public PropertyBean(Context context) {
+        this.context = context;
+        THEMES = context.getResources().getStringArray(R.array.theme);
+        DEFAULT_THEME = THEMES[0];
+        loadTheme();
+    }
+    //ËØªÂèñ‰∏ªÈ¢ò„ÄÇ‰øùÂ≠òÂú®Êñá‰ª∂configuration.cfg‰∏≠
+    private void loadTheme() {
+        Properties properties = new Properties();
+        try {
+            FileInputStream stream = context.openFileInput("configuration.cfg");
+            properties.load(stream);
+            theme = properties.getProperty("theme").toString();
+        } catch (Exception e) {
+            saveTheme(DEFAULT_THEME);
+        }
+    }
 
-	
+    //‰øùÂ≠ò‰∏ªÈ¢ò„ÄÇ‰øùÂ≠òÂú®Êñá‰ª∂configuration.cfg‰∏≠
+    private boolean saveTheme(String theme) {
+        Properties properties = new Properties();
+        properties.put("theme", theme);
+        try {
+            FileOutputStream stream = context.openFileOutput(
+                    "configuration.cfg", Context.MODE_WORLD_WRITEABLE);
+            properties.store(stream,"");
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setAndSaveTheme(String theme) {
+        this.theme = theme;
+        saveTheme(theme);
+    }
+
+
+
+
 }
